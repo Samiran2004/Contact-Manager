cbb920c37cb6d9ac5ef14bc3ca5849d195b832ff
Samiran <samirans170@gmail.com>
2024-02-26 18:57:00 +0530

    Basic setup of user router and user controller

--- controller/userController.js
+++ controller/userController.js
@@ -4,10 +4,15 @@ const registerUser = ((req, res) => {
 });
 
 const loginUser = (async (req, res) => {
-    res.status(200).send("Login User", req.body);
+    res.status(200).send("Login User");
+});
+
+const currentUser = (async(req,res)=>{
+    res.status(200).send("Current user.");
 });
 
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    currentUser
 }
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controller/userController');
+const {
+    registerUser,
+    loginUser,
+    currentUser
+} = require('../controller/userController');
 const router = express.Router();
 
 //@desc:- Signup a new user
@@ -8,6 +12,10 @@ router.post('/register', registerUser);
 
 //@desc:- Login a user
 //@route:- http://192.168.1.4:8000/api/user/login
-router.post('/login',loginUser);
+router.post('/login', loginUser);
+
+//@desc:- Current user detials
+//@route:- http://192.168.1.4:8000/api/user/current-user-detials
+router.get('/current-user-detials', currentUser);
 
 module.exports = router;
